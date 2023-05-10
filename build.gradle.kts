b6975c104d0cf53f9547fdddfc1012716a0824dc
jianxin_yang <jianxin_yang@foxitsoftware.com>
2023-05-10 14:01:58 +0800

    feat: api modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,15 @@ group = "com.vgerbot"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
-    }
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-    }
+    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
+    maven("https://maven.aliyun.com/repository/grails-core")
+    maven("https://maven.aliyun.com/repository/apache-snapshots")
+    maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+    google()
     mavenCentral()
 }
 
@@ -33,10 +36,12 @@ kotlin {
         }
     }
     sourceSets {
-        val ktorVersion = "2.3.0"
+        val ktor_version = "2.3.0"
         val commonMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-core:$ktor_version")
+                implementation("io.ktor:ktor-client-cio:$ktor_version")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
             }
         }
         val commonTest by getting {
@@ -46,13 +51,13 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-jvm:$ktorVersion")
+                implementation("io.ktor:ktor-client-jvm:$ktor_version")
             }
         }
         val jvmTest by getting
         val jsMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-js:$ktorVersion")
+                implementation("io.ktor:ktor-client-js:$ktor_version")
             }
         }
         val jsTest by getting
